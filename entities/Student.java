df6a423d351ad284b4673ae5fd988b9c8565da05
CarlosAndre2001ws <2123332100@uezo.edu.br>
2026-02-06 13:29:38 -0300

    created student class

---
+++ entities/Student.java
@@ -0,0 +1,25 @@
+package entities;
+
+public class Student {
+    private String name;
+    private int age;
+    private double height;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public Student(String name, int age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+}
