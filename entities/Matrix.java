2128eb8a5f2a6e1556a0ed127c1a2589d81fa919
CarlosAndre2001ws <2123332100@uezo.edu.br>
2026-02-11 16:56:20 -0300

    add service layer for matrix

--- entities/Matrix.java
+++ entities/Matrix.java
@@ -11,8 +11,4 @@ public class Matrix {
         this.y = y;
         matrix = new int[this.x][this.y];
     }
-
-    public int getElement(int x, int y) {
-        return this.matrix[x][y];
-    }
 }
