dbc97a3c61140d48382dd7f98121f511603421ca
CarlosAndre2001ws <2123332100@uezo.edu.br>
2026-02-11 17:05:40 -0300

    implementation of getters in Matrix class

--- entities/Matrix.java
+++ entities/Matrix.java
@@ -1,14 +1,25 @@
 package entities;
 
 public class Matrix {
-    private int x;
-    private int y;
+    private final int x;
+    private final int y;
     int[][] matrix;
-
 
     public Matrix(int x, int y) {
         this.x = x;
         this.y = y;
         matrix = new int[this.x][this.y];
+    }
+
+    public int[][] getMatrix() {
+        return this.matrix;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
     }
 }
