228bcba663d1690da475f3e1829788333834af90
CarlosAndre2001ws <2123332100@uezo.edu.br>
2026-02-11 13:23:40 -0300

    employee methods and attributes

--- entities/Employee.java
+++ entities/Employee.java
@@ -1,4 +1,54 @@
 package entities;
 
+import java.util.List;
+
 public class Employee {
+    private long id;
+    private String name;
+    private double salary;
+
+    public Employee() {}
+
+    private Employee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    private static Employee getById(List<Employee> employeeList, int id) {
+        for(Employee employee: employeeList) {
+            if(employee.id == id) {
+                return employee;
+            }
+        }
+
+        return null;
+    }
+
+    public void newEmployee(List<Employee> employeeList, int id, String name, double salary) {
+        Employee newEmployee = new Employee(id, name, salary);
+        employeeList.add(newEmployee);
+    }
+
+    public void increaseSalary(List<Employee> employeeList, int id, double perc) {
+        Employee luckyEmployee = getById(employeeList, id);
+
+        if(luckyEmployee != null) {
+            luckyEmployee.salary += luckyEmployee.salary * (perc / 100);
+        } else {
+            System.out.println("No employee found with this id, please try again.");
+        }
+    }
 }
