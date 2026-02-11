8b58b68c6643d8246f43039f2823427e7c6e4fc4
CarlosAndre2001ws <2123332100@uezo.edu.br>
2026-02-11 13:23:57 -0300

    list display in run method

--- classes/ListExercice.java
+++ classes/ListExercice.java
@@ -1,4 +1,47 @@
 package classes;
 
+import entities.Employee;
+import entities.Leitor;
+
+import javax.xml.namespace.QName;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListExercice {
+
+    public void run () {
+        int n;
+
+        Leitor sc = new Leitor();
+
+        List<Employee> employeeList = new ArrayList<>();
+
+        System.out.print("How many employees will be registered? "); n = Integer.parseInt(sc.scanner().nextLine());
+
+        for(int i = 0; i < n; i++) {
+            int id;
+            String name;
+            double salary;
+            System.out.println();
+            System.out.println("Employee #" + String.format("%.0f", (double)i + 1) + ":");
+            System.out.print("Id: "); id = Integer.parseInt(sc.scanner().nextLine());
+            System.out.print("Name: "); name = sc.scanner().nextLine();
+            System.out.print("Salary"); salary = Double.parseDouble(sc.scanner().nextLine());
+            System.out.println();
+
+            new Employee().newEmployee(employeeList, id, name, salary);
+        }
+
+        System.out.print("Enter the employee id that will have salary increase : "); int id = Integer.parseInt(sc.scanner().nextLine());
+        System.out.print("Enter the percentage"); double perc = Double.parseDouble(sc.scanner().nextLine());
+
+        new Employee().increaseSalary(employeeList, id, perc);
+
+        System.out.println("List of employees: ");
+        for(Employee employee: employeeList) {
+            System.out.println(employee.getId() + ", " + employee.getName() + ", " + employee.getSalary());
+        }
+
+        sc.fecharLeitor();
+    }
 }
