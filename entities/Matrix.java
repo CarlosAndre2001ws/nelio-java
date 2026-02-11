400dd5df13b1afb2e7e91444683dd7c3474d7bb0
CarlosAndre2001ws <2123332100@uezo.edu.br>
2026-02-11 16:55:06 -0300

    constructor and get element

--- entities/Matrix.java
+++ entities/Matrix.java
@@ -1,4 +1,18 @@
 package entities;
 
 public class Matrix {
+    private int x;
+    private int y;
+    int[][] matrix;
+
+
+    public Matrix(int x, int y) {
+        this.x = x;
+        this.y = y;
+        matrix = new int[this.x][this.y];
+    }
+
+    public int getElement(int x, int y) {
+        return this.matrix[x][y];
+    }
 }
