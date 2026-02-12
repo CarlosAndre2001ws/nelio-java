68112985843579b943fcbbb37c2a998ea6cb8789
CarlosAndre2001ws <2123332100@uezo.edu.br>
2026-02-12 15:05:00 -0300

    create in matrix services created

--- exercicio/fixacao/OcorrenciaMatriz.java
+++ exercicio/fixacao/OcorrenciaMatriz.java
@@ -1,4 +1,25 @@
 package exercicio.fixacao;
 
+import entities.Matrix;
+import services.MatrixServices;
+
 public class OcorrenciaMatriz {
+
+    public static void run() {
+        Matrix matrix = MatrixServices.create();
+
+        int x = matrix.getX();
+        int y = matrix.getY();
+
+        System.out.println(x);
+        System.out.println(y);
+
+        for(int i = 0; i < x; i++) {
+            for(int j = 0; j < y; j++) {
+                System.out.println(matrix.getMatrix()[i][j]);
+            }
+        }
+
+
+    }
 }
