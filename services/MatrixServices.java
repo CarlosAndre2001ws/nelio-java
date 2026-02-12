d9b87e59000edd3794a78af5eeb62a77f405b069
CarlosAndre2001ws <2123332100@uezo.edu.br>
2026-02-12 15:05:17 -0300

    create in matrix services implemented

--- services/MatrixServices.java
+++ services/MatrixServices.java
@@ -1,4 +1,25 @@
 package services;
 
+import entities.Leitor;
+import entities.Matrix;
+
 public class MatrixServices {
+
+    public static Matrix create() {
+
+        Leitor sc = new Leitor();
+
+        int x = Integer.parseInt(sc.scanner().nextLine());
+        int y = Integer.parseInt(sc.scanner().nextLine());
+
+        Matrix newMatrix = new Matrix(x, y);
+
+        for(int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                newMatrix.getMatrix()[i][j] = sc.scanner().nextInt();
+            }
+        }
+
+        return newMatrix;
+    }
 }
