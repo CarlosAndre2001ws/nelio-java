1a1b0987bf2e37d69225e1af2c93e66d68369bb9
Carlos André <2123332100@uezo.edu.br>
2026-02-23 13:32:51 -0300

    filled enum class

--- classes/EnumClass.java
+++ classes/EnumClass.java
@@ -1,4 +1,15 @@
 package classes;
 
+import entities.Order;
+import entities.enums.OrderStatus;
+
+import java.util.Date;
+
 public class EnumClass {
+
+    public static void run() {
+        Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+
+        System.out.println(order.toString());
+    }
 }
