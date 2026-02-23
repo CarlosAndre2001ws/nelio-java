5fca4622cff00a3a7ffa7fe4c9c1c901ed3b93c3
Carlos André <2123332100@uezo.edu.br>
2026-02-23 13:35:18 -0300

    added run class on main

--- Main.java
+++ Main.java
@@ -1,21 +1,7 @@
-import java.time.*;
-import java.time.format.DateTimeFormatter;
+import classes.EnumClass;
 
 public class Main {
     public static void main(String[] args) {
-        LocalTime time = LocalTime.now();
-        LocalDateTime dateTime = LocalDateTime.now();
-        Instant instant = Instant.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
-
-        LocalDate staticFormatedDate = LocalDate.parse("2022-07-20");
-
-        LocalDate d08 = LocalDate.parse("2022/07/20", formatter);
-
-        System.out.println("Hora atual: " + time.toString());
-        System.out.println("Data e hora atual: " + dateTime.toString());
-        System.out.println("Instante atual: " + instant.toString());
-        System.out.println("Data formatada: " + staticFormatedDate.toString());
-        System.out.println("d08 : " + d08);
+        EnumClass.run();
     }
 }
