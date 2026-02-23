178eff395419c85066f5ede1ee312d66ea75c778
Carlos André <2123332100@uezo.edu.br>
2026-02-23 13:41:06 -0300

    added valueOf method to test its functionality

--- classes/EnumClass.java
+++ classes/EnumClass.java
@@ -11,5 +11,9 @@ public class EnumClass {
         Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
 
         System.out.println(order.toString());
+
+        OrderStatus os1 = OrderStatus.DELIVERED;
+
+        OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
     }
 }
