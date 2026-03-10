539d518c98b088482a7037a95a52f83b0e64e00e
CarlosAndre2001ws <2123332100@uezo.edu.br>
2026-03-10 09:45:19 -0300

    atualizando enum

--- entities/enums/OrderStatus.java
+++ entities/enums/OrderStatus.java
@@ -2,9 +2,18 @@ package entities.enums;
 
 public enum OrderStatus {
 
-    PENDING_PAYMENT,
-    PROCESSING,
-    SHIPPED,
-    DELIVERED;
+    PENDING_PAYMENT(0),
+    PROCESSING(1),
+    SHIPPED(2),
+    DELIVERED(3);
 
+    private final int statusValue;
+
+    OrderStatus(int statusValue) {
+        this.statusValue = statusValue;
+    }
+
+    public int getStatusValue() {
+        return statusValue;
+    }
 }
