cc0a8aedbf87ed5166a32af4294b931541b6c191
CarlosAndre2001ws <2123332100@uezo.edu.br>
2026-03-10 09:48:00 -0300

    preenchendo product

--- entities/pedidoNItens/Product.java
+++ entities/pedidoNItens/Product.java
@@ -1,5 +1,22 @@
 package entities.pedidoNItens;
 
 public class Product {
+    String name;
+    Double price;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 }
